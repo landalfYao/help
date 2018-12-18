1be57e42be99489d6f0624b0db825e82c4392614
landalfYao <landalf@sunwou.com>
2018-12-18 18:22:01 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+   
+  },
+  publist(){
+    wx.navigateTo({
+      url: '/pages/pub/pub',
+    })
+  },
+  onLoad: function () {
+    
+  },
+  
+})
