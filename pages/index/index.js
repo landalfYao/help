b19d430cc884831705ed8a9656278a1ae26687ad
landalfYao <landalf@sunwou.com>
2018-12-29 18:32:14 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,10 +8,51 @@ Page({
     des:'',
     phone:''
   },
-  bindInput(e){
+
+  formSubmit(e){
     this.setData({
-      [e.currentTarget.dataset.name]:e.detail.value
+      des: e.detail.value.des,
+      phone: e.detail.value.phone,
+      price: e.detail.value.price
     })
+    if(e.detail.value.des == ''){
+      wx.showToast({
+        title: '请填写帮助内容',
+        icon:'none'
+      })
+    } else if (e.detail.value.price == ''){
+      wx.showToast({
+        title: '请输入赏金',
+        icon: 'none'
+      })
+    } else if (e.detail.value.phone == ''){
+      wx.showToast({
+        title: '请输入手机号',
+        icon: 'none'
+      })
+    }else{
+      wx.showLoading({
+        title: '加载中',
+      })
+      app.com.post('help/add',{
+        des: e.detail.value.des,
+        wx_id: wx.getStorageSync("user").id,
+        phone: e.detail.value.phone,
+        price:e.detail.value.price
+      },function(res){
+        wx.hideLoading()
+        if(res.code == 1){
+          wx.showToast({
+            title: '发布成功',
+          })
+          _this.setData({
+            des: '',
+            phone: '',
+            price: ''
+          })
+        }
+      })
+    }
   },
   navTo(e) {
     app.com.navTo(e)
